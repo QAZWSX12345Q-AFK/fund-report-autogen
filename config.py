0e267ef6702809090bc97c0680170f5acff4216f
QAZWSX12345Q-AFK <2686718691@qq.com>
2026-09-09 15:17:05 +0800

    Add FUND_CONFIG for investment funds
    Added configuration for multiple funds including their names, initial shares, daily investment amounts, and paused status.

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+FUND_CONFIG = {
+    "021778": {
+        "name": "广发纳斯达克100ETF联接F",
+        "initial_shares": 359.16,
+        "daily_invest": 100,
+        "paused": False,
+    },
+    "160213": {
+        "name": "国泰纳斯达克100",
+        "initial_shares": 190.75,
+        "daily_invest": 100,
+        "paused": False,
+    },
+    "018738": {
+        "name": "博时标普500ETF联接E",
+        "initial_shares": 269.19,
+        "daily_invest": 1500,
+        "paused": True,
+    },
+}
