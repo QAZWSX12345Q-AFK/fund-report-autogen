8d6094a0371224aa753d0c5b3c8590622280bf3c
QAZWSX12345Q-AFK <2686718691@qq.com>
2026-09-09 21:46:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,26 @@
-FUND_CONFIG = {
-    "021778": {
-        "name": "广发纳斯达克100ETF联接F",
-        "initial_shares": 359.16,
-        "daily_invest": 100,
-        "paused": False,
-        "initial_cost": 3000.00,   # ⭐ 填上你的总投入本金
+{
+  "funds": [
+    {
+      "code": "021778",
+      "name": "广发纳斯达克100",
+      "daily_amount": 100,
+      "initial_shares": 359.16,
+      "initial_cost": 3000
     },
-    "160213": {
-        "name": "国泰纳斯达克100",
-        "initial_shares": 190.75,
-        "daily_invest": 100,
-        "paused": False,
-        "initial_cost": 1050,
+    {
+      "code": "160213",
+      "name": "国泰纳斯达克100",
+      "daily_amount": 100,
+      "initial_shares": 190.75,
+      "initial_cost": 1050
     },
-    "018738": {
-        "name": "博时标普500ETF联接E",
-        "initial_shares": 269.19,
-        "daily_invest": 1500,
-        "paused": True,
-        "initial_cost": 1500.00,
-    },
+    {
+      "code": "018738",
+      "name": "博时标普500",
+      "daily_amount": 0,
+      "initial_shares": 269.19,
+      "initial_cost": 1500
+    }
+  ],
+  "start_date": "2024-01-01"
 }
