f2c68b2ac6359bb2c8dd1c0bd1f20e01598051fa
QAZWSX12345Q-AFK <2686718691@qq.com>
2026-09-09 17:57:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
-# config.py
 FUND_CONFIG = {
     "021778": {
         "name": "广发纳斯达克100ETF联接F",
         "initial_shares": 359.16,
         "daily_invest": 100,
         "paused": False,
+        "initial_cost": 3000.00,   # ⭐ 填上你的总投入本金
     },
     "160213": {
         "name": "国泰纳斯达克100",
         "initial_shares": 190.75,
         "daily_invest": 100,
         "paused": False,
+        "initial_cost": 1050,
     },
     "018738": {
         "name": "博时标普500ETF联接E",
         "initial_shares": 269.19,
         "daily_invest": 1500,
         "paused": True,
+        "initial_cost": 1500.00,
     },
 }
